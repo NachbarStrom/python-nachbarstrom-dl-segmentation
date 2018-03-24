c5fc7a93249f7eb03bffe3d737dbc9897dcfeace
tomasruizt <tomas.ruiz.te@gmail.com>
2018-03-24 19:41:18 +0100

    Add a test for the running server

---
+++ test_running_server.py
@@ -0,0 +1,24 @@
+import json
+
+import requests
+
+logcal_url = "http://localhost"
+data = {
+    "data": [
+        {"lat": 1.0, "lon": 2.0}
+    ]
+}
+response = requests.post(url=local_url, json=data)
+
+assert response.status_code == 200
+
+payload = json.loads(response.content)
+assert "data" in payload
+roofs = payload["data"]
+assert isinstance(roofs, list)
+for roof in roofs:
+    assert "roofType" in roof
+    assert "orientation" in roof
+    assert "area" in roof
+
+print("Test request ran fine: " + str(payload))
