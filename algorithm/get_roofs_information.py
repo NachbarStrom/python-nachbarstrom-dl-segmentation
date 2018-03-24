96a8661c23b41a517a503d1c799f72da03226eb0
tomasruizt <tomas.ruiz.te@gmail.com>
2018-03-24 19:50:57 +0100

    Update documentation

--- algorithm/get_roofs_information.py
+++ algorithm/get_roofs_information.py
@@ -5,9 +5,13 @@ from algorithm.cnn import cnn
 def get_roofs_information(roof_locations: Dict) -> Dict:
     """
     This function takes a dictionary from the web request and returns
-    the roof in
+    the roofs information related to
+
+    This function takes a Dict with the location of several roofs
+    and returns a Dict with the corresponding roof information for every
+    roof.
     :param roof_locations: The roof locations
-    :return:
+    :return: Roof information for every roof location
     """
     validate_roof_locations(roof_locations)
 
@@ -26,7 +30,15 @@ def get_roofs_information(roof_locations: Dict) -> Dict:
 
 
 def validate_roof_locations(roof_locations: Dict) -> None:
-    """Validates that the input is as expected."""
+    """
+    Validates that the input is as expected. Similar to:
+    {
+        "data": [
+            {"lat": 1.0, "lon": 2.0},
+            {"lat": 1.0, "lon": 2.0}
+        ]
+    }
+    """
     assert "data" in roof_locations
     roofs = roof_locations['data']
     assert isinstance(roofs, list)
