331d16d62c3213fa42645d0c5d72756ddbc53421
tomasruizt <tomas.ruiz.te@gmail.com>
2018-04-18 21:14:14 +0200

    Add '/roofs-polygons' endpoint

---
+++ roof_polygon_extractor/example_output.py
@@ -0,0 +1,17 @@
+example = \
+    {
+        "roofPolygons": [
+            {
+                "vertices": [
+                    {"lat": "1.2", "lon": "1.0"},
+                    {"lat": "1.2", "lon": "1.0"}
+                ]
+            },
+            {
+                "vertices": [
+                    {"lat": "1.2", "lon": "1.0"},
+                    {"lat": "1.2", "lon": "1.0"}
+                ]
+            }
+        ]
+    }
