70742e6e7151f6c1c9f7cf4c9c9d4ee716583fb9
tomasruizt <tomas.ruiz.te@gmail.com>
2018-05-19 19:57:14 +0200

    Update setup script with prod requirements

--- setup_ubuntu16.sh
+++ setup_ubuntu16.sh
@@ -5,8 +5,8 @@ sudo apt-get install libsm6 -y
 sudo apt-get install libgtk2.0-dev -y
 
 sudo apt-get install python3-dev python3-venv python3-pip -y
-python3 -m venv env
-source env/bin/activate
+python3 -m venv env-prod
+source env-prod/bin/activate
 
 sudo python3 -m pip install --upgrade setuptools
-sudo python3 -m pip install -r requirements.txt
+sudo python3 -m pip install -r requirements-prod.txt
